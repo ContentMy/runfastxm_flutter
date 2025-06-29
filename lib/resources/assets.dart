cf2d9fb6fca985853409ba0ab6db23e87c652d6e
ContentMy <ContentMy@163.com>
2025-06-29 23:49:57 +0800

    需求：增加资源文件与配置信息

---
+++ lib/resources/assets.dart
@@ -0,0 +1,19 @@
+// assets.dart
+class Assets {
+  Assets._();
+  ///common
+  static const String commonNavIconRemind = 'assets/icons/common/ui_nav_icon_remind.png';
+  static const String commonNavIconGoal = 'assets/icons/common/ui_nav_icon_target.png';
+  static const String commonNavIconMemorandum = 'assets/icons/common/ui_nav_icon_memorandum.png';
+  static const String commonNavIconUser = 'assets/icons/common/ui_nav_icon_user.png';
+  static const String appIcon = 'assets/icons/ic_launcher.png';
+  static const String commonImgAdd = 'assets/images/common/ui_add.png';
+  ///reminder
+  static const String reminderImgMenu = 'assets/images/reminder/ui_more_menu.png';
+  ///goal
+  static const String goalImg = 'assets/images/';
+  ///diary
+  static const String diaryImgSmail = 'assets/images/';
+  ///setting
+  static const String settingImgSmail = 'assets/images/';
+}
---
+++ lib/resources/strings.dart
@@ -0,0 +1,29 @@
+// strings.dart
+class Strings {
+  Strings._();
+  ///common
+  static const appName = 'RunFast';
+  ///guide
+  static const guideRemindTitle = '即时提醒';
+  static const guideRemindContent = '不在担心忙于其他事错过时间\n也不用烦恼扭头忘记了想要做的事情\n想做什么先把它记录下来，我来为您提醒';
+  static const guideGoalTitle = '制定目标';
+  static const guideGoalContent = '突然没有什么目标和计划心里很慌？\n又或者有想做的目标和计划总是偷懒？\n让小马为你记录你的目标，每天打卡坚持吧';
+  static const guideDiaryTitle = '记录生活';
+  static const guideDiaryContent = '生活总有美好的瞬间值得我们记录下来\n文字、心情、图片，记录生活的形式多种多样\n偶尔回过头看看，也是蛮不错的喔';
+  static const guideBeginString = '开始';
+  ///reminder
+  static const reminderTitle = '提醒列表';
+  static const reminderEmptyContent = '再也不怕忘掉啦~\n点击“+”添加提醒吧！';
+  static const reminderCompletedTitle = '已完成提醒';
+  static const reminderCompletedEmptyContent = '还没有已完成的提醒哦，快去添加提醒吧！';
+  static const reminderCreateEmptyTitle = '请输入想要提醒的事项';
+  static const reminderCreateTimePrompt = '请选择提醒时间：';
+  static const reminderCreateNullToast = '请输入想要提醒的事项后再确认哦';
+
+  ///goal
+  static const goalTitle = '目标列表';
+  ///diary
+  static const diaryTitle = '记录生活';
+  ///settings
+  static const settingsTitle = '个人设置';
+}
